9d9761cbc38b4b82025937fa9df74d982ffe4b8d
fuhao <lihinana303@gmail.com>
2022-01-18 20:59:40 +0100

    config file

---
+++ config.py
@@ -0,0 +1,9 @@
+[DATOS]
+url= https://hooks.slack.com/services/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
+direccion= /home/tfg/Escritorio/TFG/test33.log
+logtiempo=/home/tfg/Escritorio/TFG/test.log
+
+
+[MQTT]
+HOST=broker.emqx.io
+PORT= 1883
